538d37b92c3f99555f325d118c4aa252a94aa752
MKS <maks1111bm@gmail.com>
2021-07-13 23:01:38 +0300

    ADD pages Movies and Movie

--- django_movie/movies/views.py
+++ django_movie/movies/views.py
@@ -1,11 +1,23 @@
 from django.shortcuts import render
 from django.views.generic.base import View
-
+from django.views.generic.list import ListView
 from .models import Movie
 
 
 class MoviesView(View):
     """Список фильмов"""
+#    model = Movie
+#   movies = Movie.objects.all()
+#  template_name = "movies/movies.html"
+
     def get(self, request):
         movies = Movie.objects.all()
-        return render(request, "movies/movie_list.html", {"movie_list": movies})
+        return render(request, "movies/movies.html", {"movie_list": movies})
+
+
+class MovieDetailView(View):
+    """"Описание фильма"""
+    """pk - некоторое число которое мы передаем из url"""
+    def get(self, request, slug):
+        movie = Movie.objects.get(url=slug)
+        return render(request, "movies/movie_detail.html", {"movie": movie})

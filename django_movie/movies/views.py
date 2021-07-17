ec1fae9dae02d59dc5a84dac001021f6bc80731c
MKS <maks1111bm@gmail.com>
2021-07-17 14:29:58 +0300

    ADD js file and SEARCH holder

--- django_movie/movies/views.py
+++ django_movie/movies/views.py
@@ -1,24 +1,31 @@
 from django.shortcuts import render
 from django.views.generic.base import View
 from django.views.generic import ListView, DetailView
-from .models import Movie
+from .models import Movie, Actor, Genre
 
 
-class Base(View):
+class Serach(View):
     def get(self, request):
-        return render(request, "movies/base.html")
+        return render(request, "movies/movie_list.html")
 
 
-class MoviesView(ListView):
+class MoviesView(View):
     """Список фильмов"""
-    model = Movie
-    queryset = Movie.objects.filter(draft=False)
+    #model = Movie
+    #queryset = Movie.objects.filter(draft=False)
+    #template_name = "movies/example.html"
 
-    #template_name = "movies/movie_list.html"
+    def get(self, request):
+        movies = Movie.objects.all()
+        directors = Actor.objects.all()
+        genres = Genre.objects.all()
 
-    #def get(self, request):
-     #   movies = Movie.objects.all()
-      #  return render(request, "movies/movie_list.html", {"movie_list": movies})
+        return render(request, "movies/movie_list.html", {
+            "movie_list": movies,
+            "director_list": directors,
+            "genres_list": genres,
+            },
+        )
 
 
 class MovieDetailView(DetailView):
@@ -27,7 +34,7 @@ class MovieDetailView(DetailView):
     model = Movie
     slug_field = "url"
     #queryset = Movie.objects.get(url=slug)
-    #template_name = "movies/movie_list.html"
+    #template_name = "movies/example.html"
 
     #def get(self, request, slug):
      #   movie = Movie.objects.get(url=slug)
